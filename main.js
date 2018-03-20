9227f0913784ec8aeaa1ab8e95c9e6046a19a523
Brett Cimbalik <bcimbali@gmail.com>
2018-03-19 21:07:18 -0500

    added js file

---
+++ main.js
@@ -0,0 +1,14 @@
+let searchTerm = 'chicago';
+    let numberRecords = '0';
+    let startYear = '19900101';
+    let endYear = '20000101';
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + '?' + 'q=' + searchTerm + '&?page=' + numberRecords + '&begin_date=' + startYear + '&end_date=' + endYear + '&api-key=b9f91d369ff59547cd47b931d8cbc56b:0:74623931' ;
+    $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+        // Promise function with our response
+        .then(function(response) {
+          // Create a results variable and store all of the response data to it
+        console.log(response);
+    });
