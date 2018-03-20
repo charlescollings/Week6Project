b6c839955577f005820fc70438bc40c4b063da1c
Brett Cimbalik <bcimbali@gmail.com>
2018-03-19 21:14:21 -0500

    linked to the jquery

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-let searchTerm = 'chicago';
+    let searchTerm = 'chicago';
     let numberRecords = '0';
     let startYear = '19900101';
     let endYear = '20000101';
+    
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + '?' + 'q=' + searchTerm + '&?page=' + numberRecords + '&begin_date=' + startYear + '&end_date=' + endYear + '&api-key=b9f91d369ff59547cd47b931d8cbc56b:0:74623931' ;
     $.ajax({
           url: queryURL,
